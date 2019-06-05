5d45f369fffe8f60ee3b758244e47b86e3a3dbc0
Vignarajan <50335782+vignarajan@users.noreply.github.com>
2019-06-05 00:33:50 -0400

    Arrays & Conditionals

---
+++ arrays.php
@@ -0,0 +1,116 @@
+<?php
+
+/*
++---+
+| 1 |
++---+
+Declare and assign the indexed array with your favourite food 
+(at least 4 array elements). Name the array food.
+*/
+
+/*
+Print every array element in a new line.
+*/
+
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 2 |
++---+
+Print the array elements from the previous exercise in unordered list.
+<ul>
+  <li>...</li>
+  <li>...</li>
+  <li>...</li>
+  <li>...</li>
+</ul>
+*/
+
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 3 |
++---+
+Turn the indexed array food from task 1 into associative array named food_assoc. 
+Every array element of food becomes the key of food_assoc.
+Every key of food_assoc has the value that describes the type of food (salad, main course or dessert).
+*/
+
+/*
+Print every food and type in the separate lines so it renders like this:
+pizza | main counrse 
+cheesesake | desert 
+*/
+
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 4 |
++---+
+Break down the food_assoc from the task 3 to type (salad, main course, dessert, ...) 
+and country of origin (Italy, Spain, India, ...)
+food_assoc:
+  pizza:
+    type: main course
+    origin: Italy
+  cheesesake: 
+    type:desert
+    origin: Greece
+  
+So, food_assoc is still going to be an associative array. 
+Every key of food_assoc (pizza, cheesecake) will now have the value that is associative array itself 
+and carries the information about the type and origin
+*/
+
+/*
+Print every food, type and origin in the separate lines so it renders like this:
+pizza | main counrse | Italy
+cheesesake | desert | Greece
+*/
+
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/*
++---+
+| 5 |
++---+
+Print the array from task 4 in html table:
+<table>
+  <tr>
+    <th>food</th>
+    <th>type</th>
+    <th>origin</th>
+  </tr>
+  <tr>
+    <td>pizza</td>
+    <td>main course</td>
+    <td>Italy</td>
+  </tr>
+  <tr>
+    <td>cheesecake</td>
+    <td>desert</td>
+    <td>Greece</td>
+  </tr>
+</table>
+*/
+
+?>

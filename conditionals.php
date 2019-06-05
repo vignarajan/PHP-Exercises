9ea0ca18137c8fdfde97a77cf3f9561d9c6c29e3
Vignarajan <50335782+vignarajan@users.noreply.github.com>
2019-06-05 13:37:02 -0400

    Update conditionals.php

--- conditionals.php
+++ conditionals.php
@@ -7,13 +7,13 @@
 Declare variable named redLight and 
 assign it with the value true.
 */
-
+$redLight = true;
 /*
 Use "if" statement to check if the red light is on,
 if so, print "Stop the car!".
 */
 if ($redLight === true) {
-    echo "Stop the car";
+    echo "Stop the car!";
 }
 /*
 If the red light is on, use ternary operator to print "Stop the car!" as a paragraph.
@@ -31,7 +31,7 @@ echo "<hr style=\"margin 1rem 0\">";
 +---+
 Reasign redLight with the value false.
 */
-$redlight = false;
+$redLight = false;
 /*
 Use if/else statement to check if the red light is on,
 if so, print "Stop the car!", otherwise print "Are you sure that yellow light is not on?".
@@ -54,7 +54,7 @@ echo "<hr style=\"margin 1rem 0\">";
 Declare variable named yellowLight and 
 assign it with the value false.
 */
-
+$yellowLight = false;
 /*
 Use if/else statement to check if the red or yellow light is on,
 if so, print "Stop the car!", otherwise print "Are you sure that traffic lights are working at all?".
@@ -104,6 +104,7 @@ if (($redLight === true || $yellowLight === true) && $greenLight === false ) {
 } elseif (($redLight === false || $yellowLight === false) && $greenLight === true) {
     echo "Keep Moving!";
 }
+
 else {
     echo "Use the common sense and stay safe!";
 }
@@ -121,6 +122,19 @@ Handle the problem from the task 4 with switch-statement!
 The referencial value will be true.
 */
 
-
+$value = true;
+switch ($value) {
+    case (($redLight === true || $yellowLight === true) && $greenLight === false ):
+    echo "Stop the car!";
+    break;
+    case ($redLight === false && $yellowLight === false && $greenLight === false):
+    echo "Power outage!";
+    break;
+    case (($redLight === false || $yellowLight === false) && $greenLight === true):
+    echo "Keep moving!";
+    break;
+    default:
+    echo "Use the common sense and stay safe!";
+}
 
 ?>
